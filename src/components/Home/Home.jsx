a7c4ba97c96e84ba2cfff68648198f706fc6e716
ViktorZvonar <viktorzvonar@yahoo.com>
2024-03-08 16:50:19 +0200

    home ready

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Home.module.css';
 import Moderators from 'components/Moderators/Moderators';
+import Hits from 'components/Hits/Hits';
 
 function Home() {
   return (
@@ -14,6 +15,11 @@ function Home() {
           przygotowywaniu obiadów. <br /> Dania, które przygotowują są
           "okraszone" domowym ciepłem, dlatego są smaczne i zdrowe
         </p>
+      </section>
+
+      <section className={styles.moderatorsSection}>
+        <h2 className={styles.h2}>Nasze przeboje</h2>
+        <Hits />
       </section>
 
       <section className={styles.moderatorsSection}>
