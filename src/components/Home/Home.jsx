82516e0dd127bc2662f069e7bf64d61b160b337f
Viktor Zvonar <viktorzvonar@yahoo.com>
2024-07-25 23:17:20 +0300

    main info added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,10 +10,24 @@ function Home() {
         <h1 className={styles.h1}>
           Obsługujemy zarówno szkoły, firmy jak i klientów indywidualnych
         </h1>
-        <p className={styles.p}>
-          Panie pracujące w kuchni wykazują się niesamowitą zdolnością w
-          przygotowywaniu obiadów. <br /> Dania, które przygotowują są
-          "okraszone" domowym ciepłem, dlatego są smaczne i zdrowe
+        <p className={styles.info}>
+          Jesteśmy Przedsiębiorstwem Społecznym Decyzją nrD/4/2022 z dnia
+          2022–12–12 Wojewody Pomorskiego, w którym zatrudnione są osoby z
+          Centrum INTEGRACJI SPOŁECZNEJ, jak i osoby z wykluczeniem społecznym.
+          Nasza historia zaczęła się w 2018 roku z inicjatywy Stowarzyszenia
+          Eduq, które powołało do istnienia Warsztat Gastronomiczny z siedzibą w
+          Lęborku przy ul. Krzywoustego 1 (w dawnym budynku ZWAR-u) W warsztacie
+          tym znalazło zatrudnienie 5 Pań z Centrum Integracji Społecznej, które
+          to zostało powołane do istnienia przy Stowarzyszeniu Eduq. Od
+          początku, naszym klientami zostały przede wszystkim seniorzy, z
+          którymi jesteśmy, między innymi, do dnia dzisiejszego. Z czasem grono
+          klientów się poszerzało, łącznie z podopiecznymi MOPs-u. Dzisiaj nasza
+          ekipa liczy 9 osób i obsługujemy przedszkola, szkoły, firmy i osoby
+          indywidualne. Jesteśmy w trakcie remontu nowego lokalu
+          gastronomicznego, w Lęborku przy ul. Kolonia 18 i mamy nadzieję
+          poszerzyć grono naszych zadowolonych klientów, do których Was
+          zapraszamy. A zapewniamy, że warto..., bo obiady są domowe, smaczne i
+          zdrowe.
         </p>
       </section>
 
