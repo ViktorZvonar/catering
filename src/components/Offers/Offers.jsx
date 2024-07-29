42e52d8d41222b30df6c42c6a5a68b76a38d894b
Viktor Zvonar <viktorzvonar@yahoo.com>
2024-07-29 19:13:35 +0300

    some minor changes

--- src/components/Offers/Offers.jsx
+++ src/components/Offers/Offers.jsx
@@ -8,8 +8,8 @@ function Offers() {
       <section className={styles.section}>
         <h1 className={styles.h1}>Oferta</h1>
         <p className={styles.p}>
-          Wszystko to, aby sprostać Twoim oczekiwaniom i zapewnić niezapomniane
-          doznania kulinarne
+          Aby sprostać Twoim oczekiwaniom i zapewnić niezapomniane doznania
+          kulinarne
         </p>
         <p className={`${styles.p} ${styles.additionalInfo}`}>Smacznego!</p>
       </section>
