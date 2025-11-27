152e9c076d945cac2742056ce5b6d63ae1f8563a
Viktor Zvonar <108178380+ViktorZvonar@users.noreply.github.com>
2025-11-27 17:19:45 +0100

    Update contact phone number in Kontakts component

--- src/components/Kontakts/Kontakts.jsx
+++ src/components/Kontakts/Kontakts.jsx
@@ -9,7 +9,7 @@ function Kontakts() {
         <h1 className={styles.h1}>Skontaktuj się z nami</h1>
         <p className={styles.p}>ul. Krzywoustego 1, 84-300, Lębork</p>
         <p className={styles.clickableContact}>
-          <a href="tel:+48662219626"> +48 662 219 626</a>
+          <a href="tel:+48694915632"> +48 694 915 632</a>
         </p>
         <p className={`${styles.clickableContact} ${styles.last}`}>
           <a href="mailto:catering@eduqlebork.pl"> catering@eduqlebork.pl</a>
