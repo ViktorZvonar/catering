ddc52fd89b9b9691a301ff955982b35693ab8bd1
Viktor Zvonar <108178380+ViktorZvonar@users.noreply.github.com>
2025-11-27 19:57:00 +0100

    Update contact address in Kontakts component

--- src/components/Kontakts/Kontakts.jsx
+++ src/components/Kontakts/Kontakts.jsx
@@ -7,7 +7,7 @@ function Kontakts() {
     <main className={styles.main}>
       <section className={styles.firstSection}>
         <h1 className={styles.h1}>Skontaktuj się z nami</h1>
-        <p className={styles.p}>ul. Krzywoustego 1, 84-300, Lębork</p>
+        <p className={styles.p}>ul. Kolonia 18, 84-300, Lębork</p>
         <p className={styles.clickableContact}>
           <a href="tel:+48694915632"> +48 694 915 632</a>
         </p>
